a5fac3b41c373835a7e4a7eaffae38264d2a0ba5
v0idFuncti0n <lakchouchanas@gmail.com>
2020-06-05 18:49:08 +0100

    add option_image and type to option_temp

--- app/OptionTemp.php
+++ app/OptionTemp.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class OptionTemp extends Model
 {
     protected $table = "option_temp";
-    protected $fillable = ['option_text', 'point', 'question_id', 'binaire_id'];
+    protected $fillable = ['option_text', 'point', 'question_id', 'binaire_id','option_image','type'];
     protected $primaryKey = 'option_id';
     protected $dates = ['deleted_at'];
 
---
+++ database/migrations/2020_06_05_174600_add_option_image_and_type_to_option_temp.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOptionImageAndTypeToOptionTemp extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('option_temp', function (Blueprint $table) {
+            $table->string('option_image')->nullable(true);
+            $table->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('option_temp', function (Blueprint $table) {
+            $table->dropColumn('option_image');
+            $table->dropColumn('type');
+        });
+    }
+}
